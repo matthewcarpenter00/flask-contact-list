9c4a0e6cb245af21274d7fa0d84f27ed4051a7d1
matthewcarpenter00 <matthew@firstlight.media>
2021-11-19 23:33:46 +0000

    flask project

--- src/main.py
+++ src/main.py
@@ -8,7 +8,7 @@ from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User
+from models import db, Contact
 #from models import Person
 
 app = Flask(__name__)
@@ -30,14 +30,60 @@ def handle_invalid_usage(error):
 def sitemap():
     return generate_sitemap(app)
 
-@app.route('/user', methods=['GET'])
-def handle_hello():
+@app.route('/contact', methods=['GET'])
+def get_contacts():
+    contacts = Contact.query.all()
+    contactslist = list(map(lambda x: x.serialize(), contacts))
 
-    response_body = {
-        "msg": "Hello, this is your GET /user response "
-    }
+    return jsonify(contactslist), 200
 
-    return jsonify(response_body), 200
+@app.route('/contact', methods=['POST'])
+def add_contact():
+    body = request.get_json()
+    new_contact = Contact(full_name=body["full_name"], email=body["email"], phone=body["phone"], address=body["address"])
+    db.session.add(new_contact)
+    db.session.commit()
+
+    contacts = Contact.query.all()
+    contactslist = list(map(lambda x: x.serialize(), contacts))
+    return jsonify(contactslist), 200
+
+@app.route('/contact/<int:id>', methods=['PUT'])
+def edit_contact(id):
+    contact = Contact.query.get(id)
+    body = request.get_json()
+
+    if contact is None:
+        raise APIException('User not found', status_code=404)
+
+    # checking key values exists and then reassigns new value
+    if "full_name" in body:
+        contact.full_name = body["full_name"]
+    if "email" in body:
+        contact.email = body["email"]
+    if "phone" in body:
+        contact.phone = body["phone"]
+    if "address" in body:
+        contact.address = body["address"]
+    db.session.commit()
+
+    contacts = Contact.query.all()
+    contactslist = list(map(lambda x: x.serialize(), contacts))
+
+    return jsonify(contactslist), 200
+
+@app.route('/contact/<id>', methods=['DELETE'])
+def delete_contact(id):
+    contact = Contact.query.get(id)
+    if contact is None:
+        raise APIException('User not found', status_code=404)
+    db.session.delete(contact)
+    db.session.commit()
+
+    contacts = Contact.query.all()
+    contactslist = list(map(lambda x: x.serialize(), contacts))
+
+    return jsonify(contactslist), 200
 
 # this only runs if `$ python src/main.py` is executed
 if __name__ == '__main__':
--- src/models.py
+++ src/models.py
@@ -2,18 +2,54 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
-class User(db.Model):
+# here is the actual database structure
+# class User(db.Model):
+#     id = db.Column(db.Integer, primary_key=True)
+#     email = db.Column(db.String(120), unique=True, nullable=False)
+#     password = db.Column(db.String(80), unique=False, nullable=False)
+#     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+
+    # represenation method
+    # this will return a string %r is equal to what is after % - you can do multiple by adding more %r - google syntax
+    # this is mostly for us when we access a record just to see what record we are accessing
+    # in the terminal it will show as <User matthew@gmail.com>
+    # def __repr__(self):
+    #     return '<User %r>' % self.email
+
+    # serialize to convert the code to be readable and send them to the front end
+    # def serialize(self):
+    #     return {
+    #         "id": self.id,
+    #         "email": self.email,
+            # do not serialize the password, its a security breach
+        # }
+
+# this file will change the database
+# it has more impact than anything else 
+# we cant have incomplete records, we cant have overlapping records
+
+# whenever we make changes here  to the database you have to run the following commands:
+# - $ pipenv run migrate create database migrations (if models.py is edited)
+# - $ pipenv run upgrade run database migrations (if pending)
+# - $ pipenv run start start flask web server (if not running)
+# - $ pipenv run deploy deploy to heroku (if needed) 
+
+
+class Contact(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    full_name = db.Column(db.String(120), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    phone = db.Column(db.String(120), unique=True, nullable=False)
+    address = db.Column(db.String(120), unique=True, nullable=False)
 
     def __repr__(self):
-        return '<User %r>' % self.username
+        return '<User %r>' % self.full_name
 
     def serialize(self):
         return {
             "id": self.id,
+            "full_name": self.full_name,
             "email": self.email,
-            # do not serialize the password, its a security breach
+            "phone": self.phone,
+            "address": self.address
         }
